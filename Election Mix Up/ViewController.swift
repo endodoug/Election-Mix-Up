a0cdd48a6f1fa711472bdd8ba923032ff1f6b8da
Doug Harper <doug@zewkey.com>
2015-08-30 10:27:49 -0500

    Initial Commit
    Set up the project in Xcode.  Included the wordListArray and for loop.

--- Election Mix Up/ViewController.swift
+++ Election Mix Up/ViewController.swift
@@ -12,7 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var wordListArray = ["Hillary", "Jeb", "Trump", "Feel the Bern", "is", "the", "candidate", "Rand Paul", "Christie", "Republican", "Democrat"]
+        
+        view.backgroundColor = UIColor.blueColor()
+        
+        for word in wordListArray {
+            var label = UILabel()
+            label.text = word
+            label.textColor = UIColor.blueColor()
+            label.font = UIFont.systemFontOfSize(32)
+            label.sizeToFit()
+            label.backgroundColor = UIColor.whiteColor()
+            label.center = CGPoint(x: 150, y: 200)
+            
+            var randomX = CGFloat(arc4random_uniform(300))
+            var randomY = CGFloat(arc4random_uniform(480))
+            
+            label.center = CGPoint(x: randomX - 5, y: randomY)
+            
+            view.addSubview(label)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
