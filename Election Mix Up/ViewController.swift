dee020ce2f41c067386af56c5fd0084be87ae1fb
Doug Harper <doug@zewkey.com>
2015-09-01 11:59:59 -0500

    Added Custom Word functionality
    Moved the Add a New word textfield and button from bottom to top of
    screen, included the AutoLayout.  Made connections form the storyboard
    to View Controller for textfield and button. Added code to append the
    wordListArray

--- Election Mix Up/ViewController.swift
+++ Election Mix Up/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addAWordTextField: UITextField!
     
+    var wordListArray = ["Hillary", "Jeb", "Trump", "Feel the Bern", "is", "the", "candidate", "Rand Paul", "Christie", "Republican", "Democrat"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var wordListArray = ["Hillary", "Jeb", "Trump", "Feel the Bern", "is", "the", "candidate", "Rand Paul", "Christie", "Republican", "Democrat"]
+        
         
         view.backgroundColor = UIColor.blueColor()
         
@@ -31,7 +33,7 @@ class ViewController: UIViewController {
             var randomX = CGFloat(arc4random_uniform(300))
             var randomY = CGFloat(arc4random_uniform(480))
             
-            label.center = CGPoint(x: randomX - 05, y: randomY + 10)
+            label.center = CGPoint(x: randomX - 05, y: randomY + 50)
             
             view.addSubview(label)
             
@@ -58,9 +60,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func addWordTextFieldEntry(sender: UITextField) {
-        println("text entry working")
-        resignFirstResponder()
+    @IBAction func addWordEnterButtonTapped(sender: UIButton) {
+        //println("text entry working")
+        var newWordListItem = addAWordTextField.text
+        wordListArray.append(newWordListItem)
+        println(wordListArray.description)
+        addAWordTextField.resignFirstResponder()
     }
 
 }
