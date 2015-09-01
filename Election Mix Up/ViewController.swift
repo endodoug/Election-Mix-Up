d687621dec6ccd38b838e79b21fd4745191c81ea
Doug Harper <doug@zewkey.com>
2015-08-31 23:32:34 -0500

    Added a custom word feature
    Added a new label and text field for user to input their own custom
    words.  Used auto layout for position on all devices. Still need to
    hook up and write the code for adding a new word(s) to the
    wordListArray.

--- Election Mix Up/ViewController.swift
+++ Election Mix Up/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,6 +58,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addWordTextFieldEntry(sender: UITextField) {
+        println("text entry working")
+        resignFirstResponder()
+    }
 
 }
 
